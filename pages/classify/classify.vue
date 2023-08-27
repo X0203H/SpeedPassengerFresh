4088b5d561049e9d14741d94b7e0864d6994717b
X02H <2655649441@qq.com>
2023-08-27 16:39:37 +0800

    1.6更改

--- pages/classify/classify.vue
+++ pages/classify/classify.vue
@@ -1,19 +1,121 @@
 <template>
-	<view>
-		<text>分类</text>
-	</view>
+  <van-tree-select
+    :items="items"
+    height="100vh"
+    :main-active-index="mainActiveIndex"
+    :active-id="activeId"
+    @click-nav="onClickNav"
+    @click-item="onClickItem"
+  >
+    <!-- <image src="https://img.yzcdn.cn/vant/apple-1.jpg"  /> -->
+    <van-card
+      v-for="item in dataPro"
+      slot="content"
+      :desc="item.shopDesc"
+      :title="item.name"
+      :thumb="item.coverImage"
+    >
+      <view
+        slot="bottom"
+        style="
+          height: 80px;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding: 0 10px;
+          box-sizing: border-box;
+        "
+      >
+        <text style="width: 60px; display: flex; flex-wrap: wrap">
+          <text style="color: red; display: inline-block; width: 100%">{{
+            item.discountPrice
+          }}</text>
+          <text style="color: #cccccc">{{ item.marketPrice }}</text>
+        </text>
+        <text
+          style="
+            display: inline-block;
+            background-color: #924aff;
+            width: 20px;
+            height: 20px;
+            text-align: center;
+            line-height: 20px;
+            border-radius: 50%;
+            color: white;
+            font-size: 15px;
+          "
+          @click="add(item)"
+        >
+          +
+        </text>
+      </view>
+    </van-card>
+  </van-tree-select>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-
-			};
-		}
-	}
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+import http from "../../api/api.ts";
+import useStore from "../../store/index";
+const store = useStore;
+console.log(888, store);
+const dataPro = ref([]);
+const items = ref([
+  {
+    // 导航名称
+    text: "新鲜水果",
+  },
+  {
+    // 导航名称
+    text: "蔬瓜彩品",
+  },
+  {
+    // 导航名称
+    text: "满地飞禽",
+  },
+  {
+    // 导航名称
+    text: "酒水饮料",
+  },
+  {
+    // 导航名称
+    text: "海鲜水产",
+  },
+]);
+const mainActiveIndex = ref(0);
+const activeId = ref([]);
+const onClickNav = async (val) => {
+  console.log(val.detail.index);
+  const { data } = await http.get("/good/lists", {
+    page: 1,
+    limit: 20,
+    discountPrice: "asc",
+    category: val.detail.index + 1,
+  });
+  data.forEach((item) => {
+    item.quantity = 1;
+  });
+  console.log(data);
+  dataPro.value = data;
+};
+const add = (val) => {
+  console.log(val);
+  console.log(999, store);
+  store.commit("pushCar", val);
+};
+onMounted(async () => {
+  const { data } = await http.get("/good/lists", {
+    page: 1,
+    limit: 20,
+    discountPrice: "asc",
+    category: 1,
+  });
+  data.forEach((item) => {
+    item.quantity = 1;
+  });
+  dataPro.value = data;
+  console.log(1111, data);
+});
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
--- store/index.ts
+++ store/index.ts
@@ -1,32 +1,71 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 const store = createStore({
-	state: {
-		meun: [{
-			id: 1,
-			pic: '/static/images/home/menu-1.jpg',
-			name: '新鲜水果',
-		}, {
-			id: 2,
-			pic: '/static/images/home/menu-2.jpg',
-			name: '蔬瓜菜品',
-		}, {
-			id: 3,
-			pic: '/static/images/home/menu-3.jpg',
-			name: '满地飞禽',
-		}, {
-			id: 4,
-			pic: '/static/images/home/menu-4.jpg',
-			name: '酒水饮料',
-		}, {
-			id: 5,
-			pic: '/static/images/home/menu-5.jpg',
-			name: '海鲜水产',
-		}]
-	},
-	actions: {},
-	mutations: {},
-	getters: {}
-})
+  state: {
+    meun: [
+      {
+        id: 1,
+        pic: "/static/images/home/menu-1.jpg",
+        name: "新鲜水果",
+      },
+      {
+        id: 2,
+        pic: "/static/images/home/menu-2.jpg",
+        name: "蔬瓜菜品",
+      },
+      {
+        id: 3,
+        pic: "/static/images/home/menu-3.jpg",
+        name: "满地飞禽",
+      },
+      {
+        id: 4,
+        pic: "/static/images/home/menu-4.jpg",
+        name: "酒水饮料",
+      },
+      {
+        id: 5,
+        pic: "/static/images/home/menu-5.jpg",
+        name: "海鲜水产",
+      },
+    ],
+    carList: [],
+  },
+  actions: {},
+  mutations: {
+    pushCar(state, val) {
+      const isObject = (val) => typeof val === "object" && val !== null;
+      const equals = (val1, val2) => {
+        if (!isObject || !isObject(val2)) return Object.is(val1, val2);
+        if (val1 === val2) return true;
+        const val1Keys = Object.keys(val1);
+        const val2Keys = Object.keys(val2);
+        if (val1Keys.length !== val2Keys.length) return false;
+        for (let key of val1Keys) {
+          if (!val2Keys.includes(key)) {
+            return false;
+          }
+          const res = equals(val1[key], val2[key]);
+          if (!res) return false;
+        }
+        return true;
+      };
+      state.carList.push(val);
+      const newArr = [...state.carList];
+      for (let i = 0; i < newArr.length; i++) {
+        for (let j = i + 1; j < newArr.length; j++) {
+          if (equals(newArr[i], newArr[j])) {
+            newArr.splice(j, 1);
+            j--;
+            newArr[i].quantity++;
+          }
+        }
+      }
+      console.log(newArr);
+      state.carList = newArr;
+    },
+  },
+  getters: {},
+});
 
-export default store
+export default store;
